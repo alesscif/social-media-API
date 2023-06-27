3b31b61c348c8d43c228feed06141d819016573d
alesscif <alexciogli1@gmail.com>
2023-06-26 23:07:58 -0700

    completed UserMapper and added updated User entity

--- src/main/java/com/cooksys/socialmedia/entities/User.java
+++ src/main/java/com/cooksys/socialmedia/entities/User.java
@@ -4,9 +4,11 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.Table;
 
 @Entity
 @NoArgsConstructor
 @Data
+@Table(name="user_table")
 public class User {
 }
--- src/main/java/com/cooksys/socialmedia/mappers/UserMapper.java
+++ src/main/java/com/cooksys/socialmedia/mappers/UserMapper.java
@@ -1,7 +1,18 @@
 package com.cooksys.socialmedia.mappers;
 
+import com.cooksys.socialmedia.dtos.UserRequestDto;
+import com.cooksys.socialmedia.dtos.UserResponseDto;
+import com.cooksys.socialmedia.entities.User;
 import org.mapstruct.Mapper;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+
+    UserResponseDto entityToDto(User entity);
+    List<UserResponseDto> entitiesToDtos(List<User> entities);
+    User dtoToEntity(UserRequestDto dto);
+    List<User> dtostoEntities(List<UserRequestDto> dtos);
+
 }
