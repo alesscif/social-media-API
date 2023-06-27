4c21021bd4f69ba63a09e0179611e34b4aaa8bb2
alesscif <alexciogli1@gmail.com>
2023-06-27 10:37:41 -0700

    setup UserService interface methods

--- src/main/java/com/cooksys/socialmedia/services/UserService.java
+++ src/main/java/com/cooksys/socialmedia/services/UserService.java
@@ -1,4 +1,21 @@
 package com.cooksys.socialmedia.services;
 
+import com.cooksys.socialmedia.dtos.UserRequestDto;
+import com.cooksys.socialmedia.dtos.UserResponseDto;
+import com.cooksys.socialmedia.entities.User;
+
+import java.util.List;
+
 public interface UserService {
+
+    List<UserResponseDto> getAllUsers();
+    UserResponseDto createUser(UserRequestDto userToCreate);
+    UserResponseDto getUser(String username);
+    UserResponseDto updateUser(UserRequestDto updateData);
+    UserResponseDto deleteUser(UserRequestDto userToDelete);
+    void followUser(UserRequestDto userToFollow);
+    void unfollowUser(UserRequestDto userToUnfollow);
+    List<UserResponseDto> getFollowers(String username);
+    List<UserResponseDto> getFollowing(String username);
+
 }
--- src/main/java/com/cooksys/socialmedia/services/impl/UserServiceImpl.java
+++ src/main/java/com/cooksys/socialmedia/services/impl/UserServiceImpl.java
@@ -1,10 +1,58 @@
 package com.cooksys.socialmedia.services.impl;
 
+import com.cooksys.socialmedia.dtos.UserRequestDto;
+import com.cooksys.socialmedia.dtos.UserResponseDto;
 import com.cooksys.socialmedia.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+    @Override
+    public List<UserResponseDto> getAllUsers() {
+        return null;
+    }
+
+    @Override
+    public UserResponseDto createUser(UserRequestDto userToCreate) {
+        return null;
+    }
+
+    @Override
+    public UserResponseDto getUser(String username) {
+        return null;
+    }
+
+    @Override
+    public UserResponseDto updateUser(UserRequestDto updateData) {
+        return null;
+    }
+
+    @Override
+    public UserResponseDto deleteUser(UserRequestDto userToDelete) {
+        return null;
+    }
+
+    @Override
+    public void followUser(UserRequestDto userToFollow) {
+
+    }
+
+    @Override
+    public void unfollowUser(UserRequestDto userToUnfollow) {
+
+    }
+
+    @Override
+    public List<UserResponseDto> getFollowers(String username) {
+        return null;
+    }
+
+    @Override
+    public List<UserResponseDto> getFollowing(String username) {
+        return null;
+    }
 }
